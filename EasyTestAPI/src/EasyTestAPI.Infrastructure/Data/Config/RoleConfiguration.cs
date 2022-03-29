911b0b71a786de2285e0671db895c92aaf415391
mxstrong <mantas.ptakauskas@gmail.com>
2022-03-29 14:39:56 +0300

    add user and role tables and migrations

---
+++ EasyTestAPI/src/EasyTestAPI.Infrastructure/Data/Config/RoleConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using EasyTestAPI.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EasyTestAPI.Infrastructure.Data.Config;
+public class RoleConfiguration : IEntityTypeConfiguration<Role>
+{
+  public void Configure(EntityTypeBuilder<Role> builder)
+  {
+    builder.Property(r => r.RoleId).IsRequired();
+    builder.Property(r => r.Name).IsRequired();
+  }
+}
