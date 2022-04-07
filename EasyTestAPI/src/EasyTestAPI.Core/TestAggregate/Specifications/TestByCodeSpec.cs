e03c91e50b68c27f37836d5e6d1380c301b30f97
mxstrong <mantas.ptakauskas@gmail.com>
2022-04-07 13:35:20 +0300

    add method to get test by code

---
+++ EasyTestAPI/src/EasyTestAPI.Core/TestAggregate/Specifications/TestByCodeSpec.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+
+namespace EasyTestAPI.Core.TestAggregate.Specifications;
+public class TestByCodeSpec : Specification<Test>, ISingleResultSpecification
+{
+  public TestByCodeSpec(string code)
+  {
+    Query.Where(t => t.Code == code).Include(t => t.Questions)
+      .ThenInclude(q => q.Answers)
+      .Include(t => t.Questions)
+      .ThenInclude(q => q.QuestionType);
+  }
+}
